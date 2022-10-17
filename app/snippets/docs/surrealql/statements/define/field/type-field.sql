d04f457cae7043f5ab3d572cd87a7aa7fbc767dd
Matthew Ary <matthew.ary@acquisitionsimplicity.com>
2022-10-17 14:52:23 -0700

    WIP - Datatype docs Currently studying the current implmentation of data types and writing about it.

---
+++ app/snippets/docs/surrealql/statements/define/field/type-field.sql
@@ -0,0 +1,19 @@
+-- Declare the name of a field and set its type.
+DEFINE FIELD email ON TABLE user TYPE string;
+
+/**
+ * Valid types include:
+ * - any
+ * - array
+ * - bool
+ * - datetime
+ * - decimal
+ * - duration
+ * - float
+ * - int
+ * - number
+ * - object
+ * - string
+ * - record
+ * - geometry
+ */
